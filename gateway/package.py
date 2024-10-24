260c846e317bc063e1bbb4ee33ca5e3615b2ec95
Samuel Sciolla <ssciolla@umich.edu>
2024-10-24 15:32:42 -0400

    Rework DepositDirectory, Package so Package stores a reference to DepositDirectory; use forward reference for type hints

--- gateway/package.py
+++ gateway/package.py
@@ -1,24 +1,31 @@
 import os
 
+import gateway.deposit_directory
 from gateway.exceptions import NoContentException
 
 class Package:
 
-    def validate_path(self):
-        if not os.path.exists(self.root_path):
+    def get_root_path(self) -> str:
+        return self.deposit_dir.resolve(self.path)
+
+    def validate_path(self) -> None:
+        root_path = self.get_root_path()
+        if not os.path.exists(root_path):
             raise NoContentException(
-                f"No content exists at path {self.root_path}"
+                f"No content exists at path {root_path}"
             )
 
-    def __init__(self, path: str):
-        self.root_path: str = path
+    def __init__(self, deposit_dir: 'gateway.deposit_directory.DepositDirectory', path: str):
+        self.deposit_dir: 'gateway.deposit_directory.DepositDirectory' = deposit_dir
+        self.path: str = path
 
         self.validate_path()
 
     def get_file_paths(self) -> list[str]:
+        root_path = self.get_root_path()
         paths = []
-        for dirpath, _, filenames in os.walk(self.root_path):
+        for dirpath, _, filenames in os.walk(root_path):
             for filename in filenames:
                 full_path = os.path.join(dirpath, filename)
-                paths.append(os.path.relpath(full_path, self.root_path))
+                paths.append(os.path.relpath(full_path, root_path))
         return paths
