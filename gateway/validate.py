9383f946b8771f3d5587a6370d17c1e52bbac170
Jayamala <Jayamala@umich.edu>
2024-11-22 17:08:27 -0500

    Added Integration TestCase

--- gateway/validate.py
+++ gateway/validate.py
@@ -1,6 +1,12 @@
+from dataclasses import dataclass
 import subprocess
 from abc import ABC, abstractmethod
 from typing import List, Optional
+
+@dataclass
+class FixityCheckResult:
+    is_valid: bool
+    message: str
 
 class OCFLFixityValidator(ABC):
     @abstractmethod
@@ -15,17 +21,12 @@ class OCFLFixityValidator(ABC):
     def validate_multiple_objects(self, object_paths: List[str], no_fixity: bool = False, log_level: Optional[str] = None, suppress_warning: Optional[str] = None) -> str:
         pass
 
-    @abstractmethod
-    def _run_rocfl_command(self, command: List[str]) -> str:
-        pass
-
-
 class RocflOCFLFixityValidator(OCFLFixityValidator):
     def __init__(self, repository_path: str):
         self.repository_path = repository_path
 
     def validate_repository(self, no_fixity: bool = False, log_level: Optional[str] = None, suppress_warning: Optional[str] = None) -> str:
-        command = self._build_command(['rocfl', 'validate', '--repo', self.repository_path], no_fixity, log_level, suppress_warning)
+        command = self._build_command(['rocfl', 'validate', self.repository_path], no_fixity, log_level, suppress_warning)
         return self._run_rocfl_command(command)
 
     def validate_object(self, object_id: str, no_fixity: bool = False, log_level: Optional[str] = None, suppress_warning: Optional[str] = None) -> str:
@@ -43,7 +44,7 @@ class RocflOCFLFixityValidator(OCFLFixityValidator):
             )
             return result.stdout
         except subprocess.CalledProcessError as e:
-            return f"Error: {e.stderr}"
+            return f"{e.stderr}"
 
     def _build_command(self, base_command: List[str], no_fixity: bool, log_level: Optional[str], suppress_warning: Optional[str]) -> List[str]:
         if no_fixity:
@@ -51,5 +52,16 @@ class RocflOCFLFixityValidator(OCFLFixityValidator):
         if log_level:
             base_command.extend(['-l', log_level])  # Set the log level (Error, Warning, etc.)
         if suppress_warning:
-            base_command.extend(['-w', suppress_warning])  # Suppress a specific warning
+            base_command.extend(['-w', suppress_warning])  # Suppress a specific warning   
         return base_command
+    
+class FixityValidator():
+    def __init__(self, rocflvalidator: RocflOCFLFixityValidator):
+        self.rocflvalidator = rocflvalidator
+
+    def check_object_fixity(self, object_id: str) -> FixityCheckResult:
+        result = self.rocflvalidator.validate_object(object_id)
+        if "error" in result.lower():
+            return FixityCheckResult(False, result)
+        return FixityCheckResult(True, result)
+
