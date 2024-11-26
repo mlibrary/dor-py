684e2b43604d0b088e548cb732ea295d41500a08
Roger Espinosa <roger@umich.edu>
2024-11-26 09:26:06 -0500

    so much typing

--- features/steps/store_resource.py
+++ features/steps/store_resource.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Callable, Type
 
 from behave import given
@@ -8,16 +8,58 @@ class Package:
     alternate_identifier: str
 
 @dataclass
+class Resource:
+    identifier: str
+    alternate_identifier: str
+
+    # blah blah mockyrie leakage
+    @property
+    def internal_resource(self):
+        return self.__class__.name
+
+@dataclass
+class Monograph(Resource):
+    member_identifiers: list[str] = field(default_factory=list)
+
+@dataclass
+class TechnicalMetadata:
+    id: str
+    file_identifier: str
+    mimetype: str
+
+@dataclass
+class FileMetadata:
+    id: str
+    file_identifier: str
+    mimetype: str
+    technical_metdata: TechnicalMetadata
+    use: str
+
+@dataclass
+class Asset(Resource):
+    file_metadata: list[FileMetadata] = field(default_factory=list)
+
+@dataclass
 class Event:
     pass
 
 @dataclass
-class ResourceStored(Event):
-    identifier: str
-    alternate_identifier: str
+class PackageSubmitted(Event):
+    package_identifier: str
 
 @dataclass
-class Resource:
+class PackageReceived(Event):
+    package_identifier: str
+    tracking_identifier: str
+
+@dataclass
+class PackageVerified(Event):
+    identifier: str
+    alternate_identifier: str
+    resources: list[Resource]
+
+@dataclass
+class ResourceStored(Event):
     identifier: str
     alternate_identifier: str
 
@@ -43,6 +85,16 @@ class FakeMessageBus():
 def step_impl(context) -> None:
     context.package = Package(alternate_identifier="abc123youandme")
 
+    def package_submitted(event: PackageSubmitted):
+        pass
+
+    def package_received(event: PackageReceived):
+        context.identifier = 'blameitontheboogie'
+        
+    def package_verified(event: PackageVerified):
+        for path in event.paths:
+            context.repository_gateway.add(context.identifier, path)
+
     def resource_stored(event: ResourceStored):
         context.stored_event = event
 
@@ -55,6 +107,58 @@ def step_impl(context) -> None:
 @when(u'the Collection Manager places the packaged resource in the incoming location')
 def step_impl(context):
     # Roger will try to fill this out starting with comments
+
+    event = PackageSubmitted(package_identifier='2468')
+    context.message_bus.handle(event)
+    # assert?
+
+    event = PackageReceived(package_identifier=event.package_identifier, tracking_identifier='ainthtatpeculiar')
+    context.message_bus.handle(event)
+    # assert?
+
+    ## --- if you've read the deep dive, should
+    ## --- verify and process be combined?
+    event = PackageVerified(
+        identifier=context.identifier,
+        package_identifier=event.package_identifier,
+        alternate_identifier="abc123youandme",
+        resources=[
+            Monograph(
+                id=context.identifier,
+                alternate_identifier="abc123youandme",
+                member_identifiers=["asset001", "asset002"]
+            ),
+            Asset(
+                id="asset001",
+                alternate_identifier="001",
+                file_metadata=[
+                    FileMetadata(
+                        id="_0001.source",
+                        file_identifier="0001.source.jpg",
+                        mimetype="image/jpeg",
+                        technical_metadata=TechnicalMetadata(
+                            id="_0001.source.mix",
+                            file_identifier="0001.source.xml",
+                            mimetype="text/xml",
+                        ),
+                        use="preservation",
+                    )
+                ]
+            )
+        ]
+    )
+    context.message_bus.handle(event)
+    #### event = PackageProcessed(...)
+    #### context.message_bus.handle(event)
+    # assert?
+
+    # event = ResourceStored(...)
+    # context.message_bus.handle(event)
+    # assert?
+
+    # event = ResourceRegistered(...)
+    # context.message_bus.handle(event)
+
     raise NotImplementedError()
 
 @then(u'the Collection Manager can see that it was preserved')
