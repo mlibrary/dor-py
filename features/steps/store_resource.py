a2f443b5d8a96b22789cbfe4c20201324a34bb1e
Samuel Sciolla <ssciolla@umich.edu>
2024-12-02 10:55:08 -0500

    Add implementation of store_item

--- features/steps/store_resource.py
+++ features/steps/store_resource.py
@@ -1,16 +1,24 @@
-from dataclasses import dataclass, field
 import uuid
-from typing import Any, Callable, Type
+from dataclasses import dataclass, field
+from typing import Any, Callable, Set, Type
 
 from behave import given, when, then
 
 @dataclass
-class Package:
-    alternate_identifier: str
-
-@dataclass
 class Item:
     identifier: str
+
+@dataclass(frozen=True)
+class Coordinator:
+    username: str
+    email: str
+
+@dataclass(frozen=True)
+class VersionInfo():
+    coordinator: Coordinator
+    message: str
+
+# Resource
 
 @dataclass
 class Resource:
@@ -44,6 +52,8 @@ class FileMetadata:
 class Asset(Resource):
     file_metadata: list[FileMetadata] = field(default_factory=list)
 
+# Events
+
 @dataclass
 class Event:
     pass
@@ -68,6 +78,7 @@ class ItemUnpacked(Event):
     alternate_identifier: str
     resources: list[Any]
     tracking_identifier: str
+    version_info: VersionInfo
 
 @dataclass
 class ItemStored(Event):
@@ -75,19 +86,58 @@ class ItemStored(Event):
     alternate_identifier: str
     tracking_identifier: str
 
+# Gateway
+
+@dataclass
+class FakePackage:
+    alternate_identifier: str
+
+    def get_paths(self) -> list[str]:
+        return ["some/path", "some/other/path"]
+
+@dataclass
+class RepositoryObject:
+    staged_files: Set[str]
+    files: Set[str]
+
 # subclass Repository Gateway some day?
 class FakeRepositoryGateway():
 
-    def __init__(self):
-        self.store = dict()
+    def __init__(self) -> None:
+        self.store: dict[str, RepositoryObject] = dict()
+
+    def create_repository(self):
+        pass
 
     def has_object(self, object_id: str):
-        return object_id in self.store
+        return object_id in self.store and len(self.store[object_id].files) > 0
+
+    def create_staged_object(self, id: str) -> None:
+        self.store[id] = RepositoryObject(staged_files=set(), files=set())
+
+    def stage_object_files(self, id: str, source_package: FakePackage) -> None:
+        file_paths = set(source_package.get_paths())
+        if id not in self.store:
+            raise Exception()
+        self.store[id].staged_files = self.store[id].staged_files.union(file_paths)
+
+    def commit_object_changes(
+        self,
+        id: str,
+        coordinator: Coordinator,
+        message: str
+    ) -> None:
+        if id not in self.store:
+            raise Exception()
+        self.store[id].files = self.store[id].files.union(self.store[id].staged_files)
+        self.store[id].staged_files = set()
 
 class FakeUnitOfWork:
 
     def __init__(self) -> None:
         self.gateway = FakeRepositoryGateway()
+        self.gateway.create_repository()
+
         self.events: list[Event] = []
 
     def add_event(self, event: Event):
@@ -121,17 +171,19 @@ class BagAdapter():
     def is_valid(self) -> bool:
         return True
 
+# Handlers
+
 def receive_package(event: PackageReceived, uow: FakeUnitOfWork) -> None:
     event = PackageReceived(
         package_identifier=event.package_identifier,
-        tracking_identifier='ainthtatpeculiar'
+        tracking_identifier='aintthatpeculiar'
     )
     uow.add_event(event)
     # context.identifier = 'blameitontheboogie'
 
 def verify_package(event: PackageReceived, uow: FakeUnitOfWork) -> None:
     bag = BagAdapter(event.package_identifier)
-    is_valid =  bag.is_valid()
+    is_valid = bag.is_valid()
 
     if is_valid:
         uow.add_event(PackageVerified(
@@ -170,18 +222,35 @@ def unpack_item(event: PackageVerified, uow: FakeUnitOfWork) -> None:
                     )
                 ]
             )
-        ]
+        ],
+        version_info=VersionInfo(
+            coordinator=Coordinator("jermaine", "jermaine@jackson5.com"),
+            message="Just call my name, I'll be there"
+        )
     )
     uow.add_event(unpacked_event)
 
-def store_item(event: PackageVerified, uow: FakeUnitOfWork) -> None:
-    pass
-    # for path in event.paths:
-    #     context.repository_gateway.add(context.identifier, path)
+def store_item(event: ItemUnpacked, uow: FakeUnitOfWork) -> None:
+    package = FakePackage(alternate_identifier=event.alternate_identifier)
+    uow.gateway.create_staged_object(id=event.identifier)
+    uow.gateway.stage_object_files(id=event.identifier, source_package=package)
+    uow.gateway.commit_object_changes(
+        id=event.identifier,
+        coordinator=event.version_info.coordinator,
+        message=event.version_info.message
+    )
+
+    stored_event = ItemStored(
+        identifier=event.identifier,
+        alternate_identifier=event.alternate_identifier,
+        tracking_identifier=event.tracking_identifier
+    )
+    uow.add_event(stored_event)
+
+# Test
 
 @given(u'a package containing the scanned pages, OCR, and metadata')
 def step_impl(context) -> None:
-    context.package = Package(alternate_identifier="abc123youandme")
     context.uow = FakeUnitOfWork()
 
     def stored_callback(event: ItemStored, uow: FakeUnitOfWork) -> None:
