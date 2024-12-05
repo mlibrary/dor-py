91ea81a231b1811bc4c4e3c5dd6c9006102dc6de
Samuel Sciolla <ssciolla@umich.edu>
2024-12-05 11:04:27 -0500

    Let UnitOfWork be injected outside of message bus; fix some minor formatting, spacing, typing issues

--- dor/service_layer/message_bus/memory_message_bus.py
+++ dor/service_layer/message_bus/memory_message_bus.py
@@ -2,6 +2,7 @@ from typing import Callable, Type
 from dor.domain.events import Event
 from dor.service_layer.unit_of_work import UnitOfWork
 
+
 class MemoryMessageBus:
     def __init__(self, handlers: dict[Type[Event], list[Callable]]):
         # In-memory storage for event handlers
@@ -13,21 +14,21 @@ class MemoryMessageBus:
             self.event_handlers[event_type] = []
         self.event_handlers[event_type].append(handler)
 
-    def handle(self, message, uow: UnitOfWork = None):
+    def handle(self, message, uow: UnitOfWork):
         # Handles a message, which must be an event.
         if isinstance(message, Event):
             self._handle_event(message, uow)
         else:
             raise ValueError(f"Message of type {type(message)} is not a valid Event")
 
-    def _handle_event(self, event: Event, uow: UnitOfWork = None):
+    def _handle_event(self, event: Event, uow: UnitOfWork):
         # Handles an event by executing its registered handlers.
         if event.__class__ in self.event_handlers:
             queue = [event]
             while queue:
                 next_event = queue.pop(0)
                 for handler in self.event_handlers[type(next_event)]:
-                    handler(next_event, uow)
+                    handler(next_event)
                 another_event = uow.pop_event()
                 if another_event:
                     queue.append(another_event)
