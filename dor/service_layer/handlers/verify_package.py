5bfe3348249b296cacf95e5ecd4ef2769c7783e1
Roger Espinosa <roger@umich.edu>
2024-12-11 15:45:22 -0500

    extract unpack and verify handlers

---
+++ dor/service_layer/handlers/verify_package.py
@@ -0,0 +1,21 @@
+
+
+from dor.domain.events import PackageReceived, PackageVerified
+from dor.domain.models import Workspace
+from dor.service_layer.unit_of_work import UnitOfWork
+
+def verify_package(event: PackageReceived, uow: UnitOfWork, bag_reader_class: type) -> None:
+    workspace = Workspace.find(event.workspace_identifier)
+
+    bag_reader = bag_reader_class(workspace.package_directory)
+
+    is_valid = bag_reader.is_valid()
+
+    if is_valid:
+        uow.add_event(PackageVerified(
+            package_identifier=event.package_identifier,
+            tracking_identifier=event.tracking_identifier,
+            workspace_identifier=workspace.identifier,
+        ))
+    else:
+        raise Exception()
