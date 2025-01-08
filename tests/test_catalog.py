30d3d9a29dd4715a213cf767bc59c60861c583e0
Roger Espinosa <roger@umich.edu>
2025-01-08 16:55:15 -0500

    use session.begin() for transactions

--- tests/test_catalog.py
+++ tests/test_catalog.py
@@ -67,30 +67,31 @@ def sample_bin() -> Bin:
 
 def test_catalog_adds_bin(db_session, sample_bin) -> None:
     catalog = SqlalchemyCatalog(db_session)
-    with db_session:
+    with db_session.begin():
         catalog.add(sample_bin)
         db_session.commit()
 
-        rows = list(
-            db_session.execute(sqlalchemy.text("""
-                select *
-                from catalog_bin
-                where identifier = :identifier
-            """), { "identifier": "00000000-0000-0000-0000-000000000001" })
-        )
+    rows = list(
+        db_session.execute(sqlalchemy.text("""
+            select *
+            from catalog_bin
+            where identifier = :identifier
+        """), { "identifier": "00000000-0000-0000-0000-000000000001" })
+    )
     assert len(rows) == 1
     assert str(rows[0].identifier) == "00000000-0000-0000-0000-000000000001"
 
 
 def test_catalog_gets_bin(db_session, sample_bin) -> None:
     catalog = SqlalchemyCatalog(db_session)
-    with db_session:
+    with db_session.begin():
         catalog.add(sample_bin)
         db_session.commit()
 
-        bin = catalog.get("00000000-0000-0000-0000-000000000001")
-
+    bin = catalog.get("00000000-0000-0000-0000-000000000001")
+    assert bin == sample_bin
     assert bin is not None
+
     expected_bin = Bin(
         identifier=uuid.UUID("00000000-0000-0000-0000-000000000001"), 
         alternate_identifiers=["xyzzy:00000001"], 
@@ -113,6 +114,5 @@ def test_catalog_gets_bin(db_session, sample_bin) -> None:
             ]
         }
     ) 
-
 
     assert bin == expected_bin
