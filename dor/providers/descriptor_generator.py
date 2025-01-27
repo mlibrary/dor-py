59a2cb980fdd23a1ec944824b8a08235ae8147e4
Roger Espinosa <roger@umich.edu>
2025-01-27 13:56:00 -0500

    descriptor is working off list of package resources not bin

--- dor/providers/descriptor_generator.py
+++ dor/providers/descriptor_generator.py
@@ -2,22 +2,22 @@ from pathlib import Path
 from datetime import datetime, UTC
 
 from dor.settings import S, template_env
-from dor.domain.models import Bin
+from dor.providers.models import PackageResource
 
 class DescriptorGenerator:
-    def __init__(self, output_path: Path, bin: Bin):
+    def __init__(self, output_path: Path, resources: list[PackageResource]):
         self.output_path = output_path
-        self.bin = bin
+        self.resources = resources
 
     def write_files(self):
         struct_map_locref_data = {}
-        for resource in self.bin.package_resources:
+        for resource in self.resources:
             if resource.type == "Asset":
                 identifier = f"urn:dor:{resource.id}"
                 struct_map_locref_data[identifier] = f"{resource.id}.{resource.type.lower()}.mets2.xml"
 
         entity_template = template_env.get_template("preservation_mets.xml")
-        for resource in self.bin.package_resources:
+        for resource in self.resources:
             xmldata = entity_template.render(
                 resource=resource,
                 struct_map_locref_data=struct_map_locref_data,
