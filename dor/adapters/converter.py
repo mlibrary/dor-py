4c398bd7a692cea14b9e7defa42723c96fd5a539
Samuel Sciolla <ssciolla@umich.edu>
2025-03-13 11:42:45 -0400

    Move converter to separate file; replace to_jsonable_python with converter.unstructure

---
+++ dor/adapters/converter.py
@@ -0,0 +1,12 @@
+from uuid import UUID
+from datetime import datetime
+
+from cattrs import Converter
+
+
+converter = Converter()
+converter.register_unstructure_hook(datetime, lambda d: d.strftime("%Y-%m-%dT%H:%M:%SZ"))
+converter.register_structure_hook(datetime, lambda d, datetime: datetime.fromisoformat(d))
+
+converter.register_unstructure_hook(UUID, lambda u: str(u))
+converter.register_structure_hook(UUID, lambda u, UUID: UUID(u))
