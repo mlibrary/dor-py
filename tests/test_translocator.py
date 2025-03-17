cab946bbe7d183468575d7b66783fcc2588cc451
Samuel Sciolla <ssciolla@umich.edu>
2025-03-17 12:23:19 -0400

    Switch test_translocator over to new package

--- tests/test_translocator.py
+++ tests/test_translocator.py
@@ -1,24 +1,27 @@
-import pytest
-
 from pathlib import Path
+
+import pytest
 
 from dor.providers.file_system_file_provider import FilesystemFileProvider
 from dor.providers.translocator import Translocator
+
 
 @pytest.fixture
 def workspaces_path() -> Path:
     return Path("tests/test_workspaces")
 
+
 @pytest.fixture
 def inbox_path() -> Path:
     return Path("tests/fixtures/test_inbox")
+
 
 def test_create_translocator(inbox_path: Path, workspaces_path: Path) -> None:
     Translocator(
         inbox_path=inbox_path, workspaces_path=workspaces_path, minter=lambda: "some_id", file_provider=FilesystemFileProvider()
     )
- 
-    
+
+
 def test_translocator_can_create_workspace(inbox_path: Path, workspaces_path: Path) -> None:
     file_provider = FilesystemFileProvider()
     file_provider.delete_dir_and_contents(
@@ -30,7 +33,7 @@ def test_translocator_can_create_workspace(inbox_path: Path, workspaces_path: Pa
         minter=lambda: "some_id",
         file_provider=file_provider
     )
-    workspace = translocator.create_workspace_for_package("xyzzy-0001-v1")
+    workspace = translocator.create_workspace_for_package("xyzzy-00000000-0000-0000-0000-000000000001-v1")
 
     assert workspace.identifier == str(workspaces_path / "some_id")
     assert Path(workspace.identifier).exists()
