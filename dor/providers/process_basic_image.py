19c021e6df7e7002916da09ba126a68e9569f0f0
Greg Kostin <gkostin@umich.edu>
2025-04-01 15:23:25 -0400

    autopep8 formatter

--- dor/providers/process_basic_image.py
+++ dor/providers/process_basic_image.py
@@ -3,17 +3,19 @@ import shutil
 
 from dor.providers.file_system_file_provider import FilesystemFileProvider
 
+
 def data_dir(identifier):
     return Path(identifier)/"data"
+
 
 def target(identifier):
     return data_dir(identifier)/(identifier + ".function:source.known")
 
 
-def process_basic_image(identifier: str, input_path: Path, output_path: Path) -> bool: 
+def process_basic_image(identifier: str, input_path: Path, output_path: Path) -> bool:
     file_provider = FilesystemFileProvider()
     file_provider.create_directories(output_path/data_dir(identifier))
-    for filepath in  input_path.iterdir():
+    for filepath in input_path.iterdir():
         shutil.copyfile(filepath, output_path/target(identifier))
         break
     return True
