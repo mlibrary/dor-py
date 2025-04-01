0a52915e50bb7bd0f5e3a16bcf51f13e4e0175af
Greg Kostin <gkostin@umich.edu>
2025-04-01 11:58:50 -0400

    process basic image

---
+++ tests/test_file_set_processor.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+
+import pytest
+
+from dor.providers.file_system_file_provider import FilesystemFileProvider
+from dor.providers.process_basic_image import process_basic_image
+
+@pytest.fixture
+def input_path() -> Path:
+    return Path("tests/fixtures/test_basic_copy")
+
+@pytest.fixture
+def output_path() -> Path:
+    file_provider = FilesystemFileProvider()
+    output_path = Path("tests/test_process_basic_image_output")
+    file_provider.delete_dir_and_contents(output_path)
+    file_provider.create_directory(output_path)
+    return output_path
+
+
+def test_process_basic_copy_copies_input_file_to_output_file(input_path, output_path):
+    identifier = "uuid"
+    copy_of_source_file = output_path/identifier/"data"/(identifier + ".function:source.known")
+
+    success = process_basic_image(identifier, input_path, output_path)
+    
+    #     success = False
+    # for each filename in input do
+    #     copy filename to "id.function:source.known"
+    #     success = True
+    #     break
+
+    assert success
+    assert copy_of_source_file.exist()
