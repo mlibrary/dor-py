18b622da76d3a620e0793be1ac659b082873828e
Greg Kostin <gkostin@umich.edu>
2025-04-01 13:42:38 -0400

    refactor paths

--- dor/providers/process_basic_image.py
+++ dor/providers/process_basic_image.py
@@ -3,11 +3,17 @@ import shutil
 
 from dor.providers.file_system_file_provider import FilesystemFileProvider
 
+def data_dir(identifier):
+    return Path(identifier)/"data"
+
+def target(identifier):
+    return data_dir(identifier)/(identifier + ".function:source.known")
+
 
 def process_basic_image(identifier: str, input_path: Path, output_path: Path) -> bool: 
     file_provider = FilesystemFileProvider()
-    file_provider.create_directories(output_path/identifier/"data")
+    file_provider.create_directories(output_path/data_dir(identifier))
     for filepath in  input_path.iterdir():
-        shutil.copyfile(filepath, output_path/identifier/"data"/(identifier + ".function:source.known"))
+        shutil.copyfile(filepath, output_path/target(identifier))
         break
     return True
