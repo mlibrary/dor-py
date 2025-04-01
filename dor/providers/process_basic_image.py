cd278880c32aac7f6dfb8ab436cbcf7e78076083
Greg Kostin <gkostin@umich.edu>
2025-04-01 13:32:04 -0400

    simple green copy files

--- dor/providers/process_basic_image.py
+++ dor/providers/process_basic_image.py
@@ -1,5 +1,13 @@
 from pathlib import Path
+import shutil
+
+from dor.providers.file_system_file_provider import FilesystemFileProvider
 
 
 def process_basic_image(identifier: str, input_path: Path, output_path: Path) -> bool: 
-    return False
+    file_provider = FilesystemFileProvider()
+    file_provider.create_directories(output_path/identifier/"data")
+    for filepath in  input_path.iterdir():
+        shutil.copyfile(filepath, output_path/identifier/"data"/(identifier + ".function:source.known"))
+        break
+    return True
