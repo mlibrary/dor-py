204ea8f92bac2abf8996b6c0738844b6cf700da2
Greg Kostin <gkostin@umich.edu>
2025-04-01 13:53:48 -0400

    metadata test

--- tests/test_file_set_processor.py
+++ tests/test_file_set_processor.py
@@ -5,9 +5,11 @@ import pytest
 from dor.providers.file_system_file_provider import FilesystemFileProvider
 from dor.providers.process_basic_image import process_basic_image
 
+
 @pytest.fixture
 def input_path() -> Path:
     return Path("tests/fixtures/test_basic_copy")
+
 
 @pytest.fixture
 def output_path() -> Path:
@@ -24,3 +26,11 @@ def test_process_basic_copy_copies_input_file_to_output_file(input_path, output_
 
     assert process_basic_image(identifier, input_path, output_path)
     assert copy_of_source_file.exists()
+
+
+def test_process_basic_image_creates_technical_metadata(input_path, output_path):
+    identifier = "uuid"
+    technical_metadata_file = output_path/identifier/"metadata" / \
+        (identifier + ".function:service.format:image.jpg.function:technical.mix.xml")
+    assert process_basic_image(identifier, input_path, output_path)
+    assert technical_metadata_file.exists()
