1a6a16e86ee6705b5a18a2f5017d421182ceacea
Samuel Sciolla <ssciolla@umich.edu>
2025-04-02 10:32:27 -0400

    Extract get_source_file_path

--- dor/providers/process_basic_image.py
+++ dor/providers/process_basic_image.py
@@ -12,11 +12,17 @@ def target(identifier, basename):
     return data_dir(identifier) / (basename + ".function:source.known")
 
 
+def get_source_file_path(input_path: Path) -> Path:
+    for file_path in input_path.iterdir():
+        return file_path
+    raise Exception()
+
+
 def process_basic_image(identifier: str, input_path: Path, output_path: Path) -> bool:
     file_provider = FilesystemFileProvider()
     file_provider.create_directories(output_path / data_dir(identifier))
-    for filepath in input_path.iterdir():
-        basename = filepath.stem
-        shutil.copyfile(filepath, output_path / target(identifier, basename))
-        break
+
+    source_file_path = get_source_file_path(input_path)
+    basename = source_file_path.stem
+    shutil.copyfile(source_file_path, output_path / target(identifier, basename))
     return True
