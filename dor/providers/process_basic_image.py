71e30704804219bea192af0f0b68348bc6792ec9
Roger Espinosa <roger@umich.edu>
2025-04-02 13:30:41 -0400

    refactor technical metadata

--- dor/providers/process_basic_image.py
+++ dor/providers/process_basic_image.py
@@ -2,6 +2,8 @@ import shutil
 from pathlib import Path
 from typing import Callable
 
+from dataclasses import dataclass
+
 from dor.providers.file_system_file_provider import FilesystemFileProvider
 from dor.builders.parts import FileInfo, UseFunction, UseFormat
 
@@ -9,7 +11,15 @@ from dor.builders.parts import FileInfo, UseFunction, UseFormat
 class TechnicalMetadataError(Exception):
     pass
 
+@dataclass
+class FileMetaData:
+    # height: int
+    # width: int
+    mimetype: str
+    metadata: str
+    metadata_mimetype: str
 
+    
 def get_source_file_path(input_path: Path) -> Path:
     for file_path in input_path.iterdir():
         return file_path
@@ -17,7 +27,7 @@ def get_source_file_path(input_path: Path) -> Path:
 
 
 def get_fake_technical_metadata(file_path: Path) -> str:
-    return f"<xml>{file_path}</xml>"
+    return FileMetaData(mimetype="image/jpeg", metadata=f"<xml>{file_path}</xml>", metadata_mimetype="text/xml+mix")
 
 
 def process_basic_image(
@@ -34,19 +44,19 @@ def process_basic_image(
     source_file_path = get_source_file_path(input_path)
     basename = source_file_path.stem
 
+    try:
+        source_tech_metadata = get_technical_metadata(source_file_path)
+    except TechnicalMetadataError as error:
+        return False
+
     image_file_info = FileInfo(
-        identifier, basename, [UseFunction.source, UseFormat.image], "image/jpeg"
+        identifier, basename, [UseFunction.source, UseFormat.image], source_tech_metadata.mimetype
     )
     new_source_file_path = output_path / identifier / image_file_info.path
 
     shutil.copyfile(source_file_path, new_source_file_path)
 
-    try:
-        tech_xml_data = get_technical_metadata(new_source_file_path)
-    except TechnicalMetadataError as error:
-        return False
-
-    tech_meta_file_info = image_file_info.metadata(UseFunction.technical, "text/xml+mix")
-    (output_path / identifier / tech_meta_file_info.path).write_text(tech_xml_data)
+    tech_meta_file_info = image_file_info.metadata(UseFunction.technical, source_tech_metadata.metadata_mimetype)
+    (output_path / identifier / tech_meta_file_info.path).write_text(source_tech_metadata.metadata)
 
     return True
