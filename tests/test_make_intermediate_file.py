439fa09e6c4a30306e36179a3b336beaa6f10357
Samuel Sciolla <ssciolla@umich.edu>
2025-04-04 16:02:59 -0400

    Add a fixture and test case for compressed TIFF

--- tests/test_make_intermediate_file.py
+++ tests/test_make_intermediate_file.py
@@ -34,3 +34,15 @@ def test_make_intermediate_file_unrotates_tiff(fixtures_path):
 
     assert tech_metadata.mimetype == ImageMimetype.TIFF
     assert not tech_metadata.rotated
+
+
+def test_make_intermediate_file_uncompresses_tiff(fixtures_path):
+    image_path = fixtures_path / "test_image_compressed.tiff"
+    temp_file = tempfile.NamedTemporaryFile(mode='w', suffix=".tiff")
+    with temp_file:
+        temp_file_path = Path(temp_file.name)
+        make_intermediate_file(image_path, temp_file_path)
+        tech_metadata = get_technical_metadata(temp_file_path)
+
+    assert tech_metadata.mimetype == ImageMimetype.TIFF
+    assert not tech_metadata.compressed
