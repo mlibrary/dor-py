c8ad3d9d7529c74ab6650952e0b9bb4234ef1172
Samuel Sciolla <ssciolla@umich.edu>
2025-04-04 16:02:12 -0400

    Tidy up make_intermediate_file

--- dor/adapters/make_intermediate_file.py
+++ dor/adapters/make_intermediate_file.py
@@ -2,15 +2,16 @@ import subprocess
 from pathlib import Path
 
 
-def make_intermediate_file(input_path: Path, output_path: Path) -> Path:
+class IntermediateFileError(Exception):
+    pass
+
+
+def make_intermediate_file(input_path: Path, output_path: Path):
     try:
         subprocess.run(
-            ["vips", "tiffsave", f"{input_path}[autorotate]", output_path, "--compression", "none" ], 
+            ["vips", "tiffsave", f"{input_path}[autorotate]", output_path, "--compression", "none"],
             capture_output=True,
             check=True
         )
     except subprocess.CalledProcessError as e:
-        print("!", e.stderr.decode("utf-8"))
-        raise Exception
-
-    return input_path
+        raise IntermediateFileError from e
