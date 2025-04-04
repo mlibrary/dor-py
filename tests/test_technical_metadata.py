131fda387d3f76ac965b7c5530503ef71074bb68
Samuel Sciolla <ssciolla@umich.edu>
2025-04-04 11:55:28 -0400

    Introduce ImageMimeType, TechnicalMetadataMimetype enums

--- tests/test_technical_metadata.py
+++ tests/test_technical_metadata.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 import pytest
 
-from dor.adapters.technical_metadata import TechnicalMetadataError, get_technical_metadata
+from dor.adapters.technical_metadata import ImageMimetype, TechnicalMetadataMimetype, get_technical_metadata
 
 
 @pytest.fixture
@@ -13,8 +13,8 @@ def fixtures_path() -> Path:
 def test_get_technical_metadata_retrieves_data_for_jpg(fixtures_path: Path):
     image_path = fixtures_path / "test_image.jpg"
     tech_metadata = get_technical_metadata(image_path)
-    assert tech_metadata.mimetype == "image/jpeg"
-    assert tech_metadata.metadata_mimetype == "text/xml+mix"
+    assert tech_metadata.mimetype == ImageMimetype.JPEG
+    assert tech_metadata.metadata_mimetype == TechnicalMetadataMimetype.MIX
     assert not tech_metadata.rotated
     assert tech_metadata.compressed
     assert tech_metadata.metadata
@@ -23,8 +23,8 @@ def test_get_technical_metadata_retrieves_data_for_jpg(fixtures_path: Path):
 def test_get_technical_metadata_retrieves_data_for_tiff(fixtures_path: Path):
     image_path = fixtures_path / "test_image.tiff"
     tech_metadata = get_technical_metadata(image_path)
-    assert tech_metadata.mimetype == "image/tiff"
-    assert tech_metadata.metadata_mimetype == "text/xml+mix"
+    assert tech_metadata.mimetype == ImageMimetype.TIFF
+    assert tech_metadata.metadata_mimetype == TechnicalMetadataMimetype.MIX
     assert not tech_metadata.rotated
     assert not tech_metadata.compressed
     assert tech_metadata.metadata
@@ -33,8 +33,8 @@ def test_get_technical_metadata_retrieves_data_for_tiff(fixtures_path: Path):
 def test_get_technical_metadata_retrieves_data_for_rotated_tiff(fixtures_path: Path):
     image_path = fixtures_path / "test_image_rotated.tiff"
     tech_metadata = get_technical_metadata(image_path)
-    assert tech_metadata.mimetype == "image/tiff"
-    assert tech_metadata.metadata_mimetype == "text/xml+mix"
+    assert tech_metadata.mimetype == ImageMimetype.TIFF
+    assert tech_metadata.metadata_mimetype == TechnicalMetadataMimetype.MIX
     assert tech_metadata.rotated
     assert not tech_metadata.compressed
     assert tech_metadata.metadata
