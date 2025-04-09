ac438d7b0482c38bcfd4b035d3ac00497e655f15
Samuel Sciolla <ssciolla@umich.edu>
2025-04-09 16:32:42 -0400

    Introduce FileSetIdentifier to calculate identifier from project_id and basename, provide alternate_identifier

--- tests/test_file_set_processor.py
+++ tests/test_file_set_processor.py
@@ -3,17 +3,12 @@ from pathlib import Path
 import pytest
 
 from dor.providers.file_system_file_provider import FilesystemFileProvider
-from dor.providers.process_basic_image import process_basic_image
+from dor.providers.process_basic_image import FileSetIdentifier, process_basic_image
 
 
 @pytest.fixture
-def identifier() -> str:
-    return "9cdc81f5-d814-40ec-be2e-ddaf8524fcb2"
-
-
-@pytest.fixture
-def project_id() -> str:
-    return "collid"
+def file_set_identifier() -> FileSetIdentifier:
+    return FileSetIdentifier(project_id="collid", file_name="test_image.jpg")
 
 
 @pytest.fixture
@@ -30,84 +25,77 @@ def output_path() -> Path:
     return output_path
 
 
-def test_process_basic_image_copy_copies_input_file_to_output_file(identifier, project_id, input_path, output_path):
-    copy_of_source_file = output_path / identifier / "data" / ("test_image.function:source.format:image.jpg")
+def test_process_basic_image_copy_copies_input_file_to_output_file(file_set_identifier, input_path, output_path):
+    copy_of_source_file = output_path / file_set_identifier.identifier / "data" / ("test_image.function:source.format:image.jpg")
 
     assert process_basic_image(
-        identifier=identifier,
-        project_id=project_id,
+        file_set_identifier=file_set_identifier,
         input_image_path=input_path / "test_image.jpg",
         output_path=output_path
     )
     assert copy_of_source_file.exists()
 
 
-def test_process_basic_image_creates_technical_metadata(identifier, project_id, input_path, output_path):
-    technical_metadata_file = output_path / identifier / "metadata" / \
+def test_process_basic_image_creates_technical_metadata(file_set_identifier, input_path, output_path):
+    technical_metadata_file = output_path / file_set_identifier.identifier / "metadata" / \
         ("test_image.function:source.format:image.jpg.function:technical.mix.xml")
     assert process_basic_image(
-        identifier=identifier,
-        project_id=project_id,
+        file_set_identifier=file_set_identifier,
         input_image_path=input_path / "test_image.jpg",
         output_path=output_path
     )
     assert technical_metadata_file.exists()
 
 
-def test_process_basic_image_creates_service_image(identifier, project_id, input_path, output_path):
-    service_image_file = output_path / identifier / "data" / \
+def test_process_basic_image_creates_service_image(file_set_identifier, input_path, output_path):
+    service_image_file = output_path / file_set_identifier.identifier / "data" / \
         ("test_image.function:service.format:image.jp2")
     assert process_basic_image(
-        identifier=identifier,
-        project_id=project_id,
+        file_set_identifier=file_set_identifier,
         input_image_path=input_path / "test_image.jpg",
         output_path=output_path
     )
     assert service_image_file.exists()
 
 
-def test_process_basic_image_creates_service_technical_metadata(identifier, project_id, input_path, output_path):
-    technical_metadata_file = output_path / identifier / "metadata" / \
+def test_process_basic_image_creates_service_technical_metadata(file_set_identifier, input_path, output_path):
+    technical_metadata_file = output_path / file_set_identifier.identifier / "metadata" / \
         ("test_image.function:service.format:image.jp2.function:technical.mix.xml")
     assert process_basic_image(
-        identifier=identifier,
-        project_id=project_id,
+        file_set_identifier=file_set_identifier,
         input_image_path=input_path / "test_image.jpg",
         output_path=output_path
     )
     assert technical_metadata_file.exists()
 
 
-def test_process_basic_image_creates_descriptor_file(identifier, project_id, input_path, output_path):
-    descriptor_file = output_path / identifier / "descriptor" / \
-        (f"{identifier}.file_set.mets2.xml")
+def test_process_basic_image_creates_descriptor_file(file_set_identifier, input_path, output_path):
+    descriptor_file = output_path / file_set_identifier.identifier / "descriptor" / \
+        (f"{file_set_identifier.uuid}.file_set.mets2.xml")
     assert process_basic_image(
-        identifier=identifier,
-        project_id=project_id,
+        file_set_identifier=file_set_identifier,
         input_image_path=input_path / "test_image.jpg",
         output_path=output_path
     )
     assert descriptor_file.exists()
 
 
-def test_process_basic_image_creates_service_event_metadata(identifier, project_id, input_path, output_path):
-    event_metadata_file = output_path / identifier / "metadata" / \
+def test_process_basic_image_creates_service_event_metadata(file_set_identifier, input_path, output_path):
+    event_metadata_file = output_path / file_set_identifier.identifier / "metadata" / \
         ("test_image.function:service.format:image.jp2.function:event.premis.xml")
     assert process_basic_image(
-        identifier=identifier,
-        project_id=project_id,
+        file_set_identifier=file_set_identifier,
         input_image_path=input_path / "test_image.jpg",
         output_path=output_path
     )
     assert event_metadata_file.exists()
 
 
-def test_process_basic_image_creates_source_event_metadata(identifier, project_id, input_path, output_path):
-    event_metadata_file = output_path / identifier / "metadata" / \
+def test_process_basic_image_creates_source_event_metadata(file_set_identifier, input_path, output_path):
+    event_metadata_file = output_path / file_set_identifier.identifier / "metadata" / \
         ("test_image.function:source.format:image.jpg.function:event.premis.xml")
     assert process_basic_image(
-        identifier=identifier,
-        project_id=project_id,
+        file_set_identifier=file_set_identifier,
         input_image_path=input_path / "test_image.jpg",
         output_path=output_path
     )
