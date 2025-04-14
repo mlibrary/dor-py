7f230185ef64c9b228acb810966b9ebb12b5d2df
Samuel Sciolla <ssciolla@umich.edu>
2025-04-14 11:34:57 -0400

    Extract some functions

--- dor/providers/process_basic_image.py
+++ dor/providers/process_basic_image.py
@@ -2,6 +2,7 @@ import hashlib
 import shutil
 import tempfile
 import uuid
+from dataclasses import dataclass
 from datetime import UTC, datetime
 from pathlib import Path
 from typing import Callable
@@ -97,6 +98,90 @@ def create_file_set_directories(file_set_directory: Path) -> None:
     file_provider.create_directory(file_set_directory / "descriptor")
 
 
+def get_source_file_info(
+    file_set_identifier: FileSetIdentifier, tech_metadata: TechnicalMetadata
+) -> FileInfo:
+    return FileInfo(
+        identifier=file_set_identifier.identifier,
+        basename=file_set_identifier.basename,
+        uses=[UseFunction.source, UseFormat.image],
+        mimetype=tech_metadata.mimetype.value
+    )
+
+
+def get_service_file_info(file_set_identifier: FileSetIdentifier) -> FileInfo:
+    return FileInfo(
+        identifier=file_set_identifier.identifier,
+        basename=file_set_identifier.basename,
+        uses=[UseFunction.service, UseFormat.image],
+        mimetype=ImageMimetype.JP2.value
+    )
+
+
+def copy_source_file(source_path: Path, destination_path: Path) -> None:
+    shutil.copyfile(source_path, destination_path)
+
+
+def get_event_file_info(file_info: FileInfo):
+    return file_info.metadata(use=UseFunction.event, mimetype="text/xml+premis")
+
+
+def create_service_file(
+    source_path: Path,
+    destination_path: Path,
+    tech_metadata: TechnicalMetadata,
+    generate_service_variant: Callable[[Path, Path], None]
+) -> None:
+    temp_file = tempfile.NamedTemporaryFile(mode='w', suffix=".tiff")
+    with temp_file:
+        if tech_metadata.compressed or tech_metadata.rotated:
+            compressible_file_path = Path(temp_file.name)
+            make_intermediate_file(source_path, compressible_file_path)
+        else:
+            compressible_file_path = source_path
+
+        generate_service_variant(compressible_file_path, destination_path)
+
+
+@dataclass
+class FileInfoAssociation:
+    file_info: FileInfo
+    tech_metadata_file_info: MetadataFileInfo
+    source_file_info: FileInfo | None = None
+
+
+def create_package_resource(
+    file_set_identifier: FileSetIdentifier,
+    metadata_file_infos: list[MetadataFileInfo],
+    file_info_associations: list[FileInfoAssociation]
+) -> PackageResource:
+    file_metadatas = []
+    for file_info_association in file_info_associations:
+        file_metadatas.append(FileMetadata(
+            id=file_info_association.file_info.xmlid,
+            use=flatten_use(*file_info_association.file_info.uses),
+            groupid=file_info_association.source_file_info.xmlid if file_info_association.source_file_info is not None else None,
+            mdid=file_info_association.tech_metadata_file_info.xmlid,
+            ref=FileReference(
+                locref=file_info_association.file_info.locref,
+                mimetype=file_info_association.file_info.mimetype
+            )
+        ))
+
+    resource = PackageResource(
+        id=file_set_identifier.uuid,
+        type="File Set",
+        alternate_identifier=AlternateIdentifier(id=file_set_identifier.alternate_identifier, type="DLXS"),
+        events=[],
+        metadata_files=[
+            convert_metadata_file_info_to_file_metadata(metadata_file_info)
+            for metadata_file_info in metadata_file_infos
+        ],
+        data_files=file_metadatas
+    )
+    return resource
+
+
 def process_basic_image(
     file_set_identifier: FileSetIdentifier,
     image_path: Path,
@@ -115,20 +200,14 @@ def process_basic_image(
     if source_tech_metadata.mimetype not in ACCEPTED_IMAGE_MIMETYPES:
         return False
 
-    source_file_info = FileInfo(
-        identifier=file_set_identifier.identifier,
-        basename=file_set_identifier.basename,
-        uses=[UseFunction.source, UseFormat.image],
-        mimetype=source_tech_metadata.mimetype.value
-    )
+    source_file_info = get_source_file_info(file_set_identifier, source_tech_metadata)
     source_file_path = file_set_directory / source_file_info.path
-    shutil.copyfile(image_path, source_file_path)
+    source_event_metadata_file_info = get_event_file_info(source_file_info)
+
+    copy_source_file(source_path=image_path, destination_path=source_file_path)
 
     source_event = create_preservation_event("copy source image", collection_manager_email)
     source_event_xml = PreservationEventSerializer(source_event).serialize()
-    source_event_metadata_file_info = source_file_info.metadata(
-        use=UseFunction.event, mimetype="text/xml+premis"
-    )
     (file_set_directory / source_event_metadata_file_info.path).write_text(source_event_xml)
 
     source_tech_metadata_file_info = source_file_info.metadata(
@@ -136,32 +215,22 @@ def process_basic_image(
     )
     (file_set_directory / source_tech_metadata_file_info.path).write_text(str(source_tech_metadata))
 
-    service_file_info = FileInfo(
-        identifier=file_set_identifier.identifier,
-        basename=file_set_identifier.basename,
-        uses=[UseFunction.service, UseFormat.image],
-        mimetype=ImageMimetype.JP2.value
-    )
+    service_file_info = get_service_file_info(file_set_identifier)
     service_file_path = file_set_directory / service_file_info.path
+    service_event_metadata_file_info = get_event_file_info(service_file_info)
 
-    temp_file = tempfile.NamedTemporaryFile(mode='w', suffix=".tiff")
-    with temp_file:
-        if source_tech_metadata.compressed or source_tech_metadata.rotated:
-            compressible_file_path = Path(temp_file.name)
-            make_intermediate_file(source_file_path, compressible_file_path)
-        else:
-            compressible_file_path = source_file_path
-
-        try:
-            generate_service_variant(compressible_file_path, service_file_path)
-        except ServiceImageProcessingError:
-            return False
+    try:
+        create_service_file(
+            source_path=source_file_path,
+            destination_path=service_file_path,
+            tech_metadata=source_tech_metadata,
+            generate_service_variant=generate_service_variant
+        )
+    except ServiceImageProcessingError:
+        return False
 
     service_event = create_preservation_event("generate service derivative", collection_manager_email)
     service_event_xml = PreservationEventSerializer(service_event).serialize()
-    service_event_metadata_file_info = service_file_info.metadata(
-        use=UseFunction.event, mimetype="text/xml+premis"
-    )
     (file_set_directory / service_event_metadata_file_info.path).write_text(service_event_xml)
 
     try:
@@ -174,37 +243,24 @@ def process_basic_image(
     )
     (file_set_directory / service_tech_metadata_file_info.path).write_text(str(service_tech_metadata))
 
-    resource = PackageResource(
-        id=file_set_identifier.uuid,
-        type="File Set",
-        alternate_identifier=AlternateIdentifier(id=file_set_identifier.alternate_identifier, type="DLXS"),
-        events=[],
-        metadata_files=[
-            convert_metadata_file_info_to_file_metadata(source_tech_metadata_file_info),
-            convert_metadata_file_info_to_file_metadata(source_event_metadata_file_info),
-            convert_metadata_file_info_to_file_metadata(service_tech_metadata_file_info),
-            convert_metadata_file_info_to_file_metadata(service_event_metadata_file_info)
+    resource = create_package_resource(
+        file_set_identifier=file_set_identifier,
+        metadata_file_infos=[
+            source_tech_metadata_file_info,
+            source_event_metadata_file_info,
+            service_tech_metadata_file_info,
+            service_event_metadata_file_info
         ],
-        data_files=[
-            FileMetadata(
-                id=source_file_info.xmlid,
-                use=flatten_use(*source_file_info.uses),
-                mdid=source_tech_metadata_file_info.xmlid,
-                ref=FileReference(
-                    locref=source_file_info.locref,
-                    mimetype=source_file_info.mimetype
-                )
+        file_info_associations=[
+            FileInfoAssociation(
+                file_info=source_file_info,
+                tech_metadata_file_info=source_tech_metadata_file_info,
             ),
-            FileMetadata(
-                id=service_file_info.xmlid,
-                use=flatten_use(*service_file_info.uses),
-                groupid=source_file_info.xmlid,
-                mdid=service_tech_metadata_file_info.xmlid,
-                ref=FileReference(
-                    locref=service_file_info.locref,
-                    mimetype=service_file_info.mimetype
-                )
-            ),
+            FileInfoAssociation(
+                file_info=service_file_info,
+                tech_metadata_file_info=service_tech_metadata_file_info,
+                source_file_info=source_file_info
+            )
         ]
     )
     descriptor_xml = create_file_set_descriptor_data(resource)
