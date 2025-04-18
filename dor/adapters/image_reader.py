c1b27b5df3e7dd08fdb8cebc5e514bb34dc9523d
Samuel Sciolla <ssciolla@umich.edu>
2025-04-18 16:37:28 -0400

    retrieve_int -> retrieve_attribute_value

--- dor/adapters/image_reader.py
+++ dor/adapters/image_reader.py
@@ -57,28 +57,18 @@ class AltoDoc:
     def __str__(self) -> str:
         return ET.tostring(self.tree, encoding="unicode")
 
-    @property
-    def page_dimensions(self) -> dict[str, int]:
-        page_elem = self.tree.find(".//alto:Page", self.ns_map)
-        if page_elem is None: raise AltoDocError
-        width = page_elem.get("WIDTH")
-        if width is None: raise AltoDocError
-        height = page_elem.get("HEIGHT")
-        if height is None: raise AltoDocError
-        return {"width": int(width), "height": int(height)}
-
     @staticmethod
-    def retrieve_int(elem: ET.Element, attribute: str):
+    def retrieve_attribute_value(elem: ET.Element, attribute: str):
         value = elem.get(attribute)
         if value is None: raise AltoDocError
-        return int(value)
+        return value
 
     @staticmethod
     def get_word_coordinates(elem: ET.Element) -> list[int]:
-        hpos = AltoDoc.retrieve_int(elem, "HPOS")
-        vpos = AltoDoc.retrieve_int(elem, "VPOS")
-        width = AltoDoc.retrieve_int(elem, "WIDTH")
-        height = AltoDoc.retrieve_int(elem, "HEIGHT")
+        hpos = int(AltoDoc.retrieve_attribute_value(elem, "HPOS"))
+        vpos = int(AltoDoc.retrieve_attribute_value(elem, "VPOS"))
+        width = int(AltoDoc.retrieve_attribute_value(elem, "WIDTH"))
+        height = int(AltoDoc.retrieve_attribute_value(elem, "HEIGHT"))
         coordinates = [hpos, vpos, hpos + width, vpos + height]
         return coordinates
 
@@ -87,13 +77,20 @@ class AltoDoc:
         return AltoDoc.strip_punctuation_pattern.sub("", word)
 
     @property
+    def page_dimensions(self) -> dict[str, int]:
+        page_elem = self.tree.find(".//alto:Page", self.ns_map)
+        if page_elem is None: raise AltoDocError
+        width = int(self.retrieve_attribute_value(page_elem, "WIDTH"))
+        height = int(self.retrieve_attribute_value(page_elem, "HEIGHT"))
+        return {"width": width, "height": height}
+
+    @property
     def word_data(self):
         word_data = {}
         word_elems = self.tree.findall(".//alto:String", self.ns_map)
         for word_elem in word_elems:
             coordinates = self.get_word_coordinates(word_elem)
-            word = word_elem.get("CONTENT")
-            if word is None: raise AltoDocError
+            word = self.retrieve_attribute_value(word_elem, "CONTENT")
             normalized_word = self.strip_punctuation(word.lower())
             if normalized_word in word_data:
                 word_data[normalized_word].append(coordinates)
