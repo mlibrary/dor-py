cb48fdc705330bf52bad464704e130c6c3e1e41b
Samuel Sciolla <ssciolla@umich.edu>
2025-04-24 17:03:29 -0400

    Add rough draft of ExtractImageText operation and tests

--- dor/builders/parts.py
+++ dor/builders/parts.py
@@ -180,7 +180,9 @@ class FileInfo:
                 return "jpg"
             case "image/tiff":
                 return "tiff"
-            case "text/plain":
+            case "text/plain; charset=US-ASCII":
+                return "txt"
+            case "text/plain; charset=UTF-8":
                 return "txt"
             case "text/xml+premis":
                 return "premis.xml"
