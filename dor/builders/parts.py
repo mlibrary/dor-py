36f75dfe1768423b6cdbdc328605800d2c213cd4
Samuel Sciolla <ssciolla@umich.edu>
2025-04-28 10:46:06 -0400

    Add text/plain mimetype

--- dor/builders/parts.py
+++ dor/builders/parts.py
@@ -243,6 +243,8 @@ class FileInfo:
                 return "jpg"
             case "image/tiff":
                 return "tiff"
+            case "text/plain":
+                return "txt"
             case "text/plain; charset=US-ASCII":
                 return "txt"
             case "text/plain; charset=UTF-8":
