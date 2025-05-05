67a25ce82aac219bded8ae4553ec8c507ad06948
Samuel Sciolla <ssciolla@umich.edu>
2025-05-05 16:53:00 -0400

    Create OPClient ABC; create test case with referenced file set; modify PackageGenerator to accept OPClient and search for file sets

---
+++ dor/providers/op_client.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+
+
+@dataclass
+class FileSetSearchResult:
+    file_set_identifier: str
+    bin_identifier: str
+
+
+class OPClient(ABC):
+
+    @abstractmethod
+    def search_for_file_set(self, file_set_identifier: str) -> FileSetSearchResult | None:
+        raise NotImplementedError
