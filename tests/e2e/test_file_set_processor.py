08c6996d41b7eafdfe6a59171ee5b496e8139702
Greg Kostin <gkostin@umich.edu>
2025-05-14 14:30:43 -0400

    call build_file_set from creates_a_file_set_from_uploaded_materials

--- tests/e2e/test_file_set_processor.py
+++ tests/e2e/test_file_set_processor.py
@@ -1,5 +1,29 @@
 import pytest
 
+
+# 00001.tif
+# 00001.txt
+#
+# -> { "0001": [ "0001.tif", "0001.txt" ] }
+#
+# op = { "0001.tif": "basic-image", "0001.txt": "append-service" }
+#
+# POST /upload?name=0001&files=[]&config=
+#
+# {
+# 	"image/*": ["CompressSourceImage"],
+# 	"text/*": [
+# 		"AppendUses", {
+# 			"target": {
+# 				"function": ["function:source"],
+# 				"format": "format:text-plain"
+# 			},
+# 			"uses": [ "function:service" ]
+# 		}
+# 	]
+# }
+
+
 @pytest.mark.usefixtures("test_client")
 def test_filesets_api_returns_200_and_summary(
     test_client
@@ -19,7 +43,8 @@ def test_filesets_api_returns_200_and_summary(
     data = {
         "name": "test_image",
         "project_id": "today",
-        "profile": "basic-image",
+        "profile": "basic-image"
+        # "profile": { "test_image.jpg": "basic-image" },
     }
     response = test_client.post("api/v1/filesets", files=upload_files, data=data)
 
