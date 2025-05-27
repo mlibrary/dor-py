710533df99e35f1a3a2d7d4630677cf074959dbb
Samuel Sciolla <ssciolla@umich.edu>
2025-05-27 16:39:03 -0400

    Return rudimentary response data

--- dor/entrypoints/api/packages.py
+++ dor/entrypoints/api/packages.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from dataclasses import dataclass
 from typing import Annotated
 
 from fastapi import APIRouter, Body
@@ -11,11 +12,17 @@ from dor.queues import queues
 packages_router = APIRouter(prefix="/packages")
 
 
+@dataclass
+class PackageResponse:
+    identifier: str
+    deposit_group_identifier: str
+
+
 @packages_router.post("/")
 async def create_package(
     package_metadata: Annotated[dict, Body(...)],
     deposit_group: Annotated[dict[str, str], Body(...)]
-):
+) -> PackageResponse:
     deposit_group_ = DepositGroup(
         deposit_group["identifier"],
         datetime.fromisoformat(deposit_group["date"])
@@ -27,4 +34,7 @@ async def create_package(
         package_metadata
     )
 
-    return {}
+    return PackageResponse(
+        identifier=package_metadata["identifier"],
+        deposit_group_identifier=deposit_group_.identifier
+    )
