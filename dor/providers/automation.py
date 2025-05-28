a97295a817d5ea623e6a6bf3b1a8e8ee9a19ff9e
Samuel Sciolla <ssciolla@umich.edu>
2025-05-28 13:47:35 -0400

    args -> kwargs

--- dor/providers/automation.py
+++ dor/providers/automation.py
@@ -1,19 +1,18 @@
-from typing import Tuple
-
 from dor.queues import queues
+from tests.test_package_generator import deposit_group
 
 
-def run_automation(event: str, *args):
+def run_automation(event: str, **kwargs):
 
     match event:
         case "package.create":
             from dor.providers.packages import create_package_from_metadata
             queues["package"].enqueue(
                 create_package_from_metadata,
-                *args
+                **kwargs
             )
         case "package.success":
-            print(*args)
+            print(kwargs.get("package_identifier"))
         case _:
             raise Exception
     
