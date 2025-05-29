f87b4e56aa81443483d7252462014df9c27e0272
Samuel Sciolla <ssciolla@umich.edu>
2025-05-29 11:58:13 -0400

    Add handling for path that does not exist, with test

--- dor/cli/package.py
+++ dor/cli/package.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from typing import Annotated
 
 import typer
@@ -10,4 +11,9 @@ package_app = typer.Typer()
 def upload(
     packet: Annotated[str, typer.Argument(help="Path to a JSONL packet file.")]
 ):
+    packet_path = Path(packet)
+    if not packet_path.exists():
+        typer.secho("Packet file was not found at the path provided.", fg=typer.colors.RED)
+        raise typer.Exit(1)
+
     print(packet)
