35397c68dc9d0281019a70cc13e8c0cbe5425c5d
Greg Kostin <gkostin@umich.edu>
2025-05-29 11:09:45 -0400

    uncomment PackageGenerator

--- dor/providers/packages.py
+++ dor/providers/packages.py
@@ -1,13 +1,12 @@
 from typing import Any
-# from datetime import datetime, UTC
+from datetime import datetime, UTC
 from pathlib import Path
 
-# from dor.config import config
 from dor.providers.automation import run_automation
+from dor.providers.file_system_file_provider import FilesystemFileProvider
+from dor.providers.repository_client import FakeRepositoryClient
+from dor.providers.package_generator import (DepositGroup, PackageGenerator)
 from dor.queues import queues
-# from dor.providers.file_system_file_provider import FilesystemFileProvider
-from dor.providers.package_generator import (DepositGroup, ) # PackageGenerator
-# from dor.providers.repository_client import FakeRepositoryClient
 
 
 def create_package_from_metadata(
@@ -23,15 +22,14 @@ def create_package_from_metadata(
 
     identifier = package_metadata["identifier"]
 
-    # TODO: Uncomment once PackageGenerator is updated to handle fileset paths
-    # PackageGenerator(
-    #     file_provider=FilesystemFileProvider(),
-    #     repository_client=FakeRepositoryClient(),
-    #     metadata=package_metadata,
-    #     deposit_group=deposit_group,
-    #     output_path=inbox_path,
-    #     file_sets_path=pending_path,
-    #     timestamp=datetime.now(tz=UTC)
-    # ).generate()
+    PackageGenerator(
+        file_provider=FilesystemFileProvider(),
+        repository_client=FakeRepositoryClient(),
+        metadata=package_metadata,
+        deposit_group=deposit_group,
+        output_path=inbox_path,
+        file_sets_path=pending_path,
+        timestamp=datetime.now(tz=UTC)
+    ).generate()
 
     queues["automation"].enqueue(run_automation, "package.success", package_identifier=identifier)
