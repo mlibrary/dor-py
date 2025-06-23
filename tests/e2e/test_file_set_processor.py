a69ac6e26a392d20fdb94fc605152b815a8a43c5
Greg Kostin <gkostin@umich.edu>
2025-06-23 14:17:29 -0400

    use string literals in test

--- tests/e2e/test_file_set_processor.py
+++ tests/e2e/test_file_set_processor.py
@@ -12,64 +12,60 @@ from dor.providers.file_system_file_provider import FilesystemFileProvider
 def test_filesets_api_returns_200_and_summary(
     test_client
 ) -> None:
-    fileset_helper = FileSetIdentifier(project_id="collid", file_name="test_image.jpg")
-    fileset_path = Path(f"{config.filesets_path}/{fileset_helper.identifier}")
     file_provider = FilesystemFileProvider()
-    file_provider.delete_dir_and_contents(path=fileset_path)
-    file_path = f"tests/fixtures/test_file_set_processor/{fileset_helper.file_name}"
+    file_provider.delete_dir_and_contents(path=Path(f"{config.filesets_path}/c6d9deda-03b9-a701-1613-6fedac69d6ba"))
+    file_path = "tests/fixtures/test_file_set_processor/test_image.jpg"
     upload_files = []
     upload_files.append(
         (
             "files",
             (
-                fileset_helper.file_name,
+                "test_image.jpg",
                 open(file_path, "rb"),
                 "image/jpeg",
             ),
         )
     )
     data = {
-        "name": fileset_helper.basename,
-        "project_id": fileset_helper.project_id,
+        "name": "test_image",
+        "project_id": "collid",
         "file_profiles": json.dumps({
-            f"{fileset_helper.file_name}": ["compress-source"]
+            "test_image.jpg": ["compress-source"]
         })
     }
     response = test_client.post("api/v1/filesets", files=upload_files, data=data)
 
     assert response.status_code == 200
-    assert fileset_path.exists()
+    assert Path(f"{config.filesets_path}/c6d9deda-03b9-a701-1613-6fedac69d6ba").exists()
 
 
 @pytest.mark.usefixtures("test_client")
 def test_filesets_api_returns_200_and_summary_for_image_with_ocr(
     test_client
 ) -> None:
-    fileset_helper = FileSetIdentifier(project_id="collid", file_name="quick-brown.tiff")
-    fileset_path = Path(f"{config.filesets_path}/{fileset_helper.identifier}")
     file_provider = FilesystemFileProvider()
-    file_provider.delete_dir_and_contents(path=fileset_path)
-    file_path = f"tests/fixtures/test_file_set_processor_with_ocr/{fileset_helper.file_name}"
+    file_provider.delete_dir_and_contents(path=Path(f"{config.filesets_path}/faae8c28-642f-e921-15ed-9fa8669354df"))
+    file_path = "tests/fixtures/test_file_set_processor_with_ocr/quick-brown.tiff"
     upload_files = []
     upload_files.append(
         (
             "files",
             (
-                fileset_helper.file_name,
+                "quick-brown.tiff",
                 open(file_path, "rb"),
                 "image/tiff",
             ),
         )
     )
     data = {
-        "name": fileset_helper.basename,
-        "project_id": fileset_helper.project_id,
+        "name": "quick-brown",
+        "project_id": "collid",
         "file_profiles": json.dumps({
-            f"{fileset_helper.file_name}": ["compress-source", "extract-text"]
+            "quick-brown.tiff": ["compress-source", "extract-text"]
         })
     }
 
     response = test_client.post("api/v1/filesets", files=upload_files, data=data)
 
     assert response.status_code == 200
-    assert fileset_path.exists()
+    assert Path(f"{config.filesets_path}/faae8c28-642f-e921-15ed-9fa8669354df").exists()
